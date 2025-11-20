900b73bd3caac9ca493597a27e564a62edb16d89
unknown <sepehr.msepehrprog@gmail.com>
2025-11-20 16:52:08 +0330

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+text_menu = """
+===========================================
+        LOCAL API TESTING CLI TOOL
+===========================================
+
+[1] Send a Request
+[2] Manage Saved Requests
+[3] View Request History
+[4] Settings
+[0] Exit
+
+-------------------------------------------
+Select an option:
+
+"""
+
+while True:
+    print(">")
+    command = input(text_menu)
